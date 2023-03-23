062ca4ecfcef2a653c039a99af1afe698b0bcde6
Camila Souza <44167270+camilaesouza@users.noreply.github.com>
2023-03-23 20:08:57 -0300

    setup firebase (#8)
    * setup firebase
    
    * updated action
    
    ---------
    
    Co-authored-by: andres <andres@pop-os.localdomain>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import firebaseConfig from "./config/firebaseConfig";
+import useFirebase from "./hooks/useFirebase";
+import Home from "./pages/Home/index.page";
 
 function App() {
-  return <div>Hello SD</div>;
+  const app = useFirebase(firebaseConfig);
+
+  if (!app) return <div>Loading</div>;
+
+  return <Home></Home>;
 }
 
 export default App;
---
+++ src/config/firebaseConfig.example.ts
@@ -0,0 +1,12 @@
+import { FirebaseOptions } from "firebase/app";
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+export default firebaseConfig;
