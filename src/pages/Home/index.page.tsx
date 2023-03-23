f1b7421124be11fdd1593531b663023436786ccc
Samuel <samboeira@gmail.com>
2023-03-23 20:16:33 -0300

    setup ui

--- src/pages/Home/index.page.tsx
+++ src/pages/Home/index.page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Button } from "@mantine/core";
 
 export default function Home() {
-  return <div>index.page</div>;
+  return (
+    <div>
+      index.page
+      <Button>Settings</Button>
+    </div>
+  );
 }
