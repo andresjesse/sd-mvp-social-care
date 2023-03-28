8bb6959d93e10ffbd8cf90d04b95a6faa8d3bd5e
Camila Souza <camila-emanuele@hotmail.com>
2023-03-28 20:00:19 -0300

    config files from i18n

---
+++ src/lang/index.ts
@@ -0,0 +1,7 @@
+import { I18n } from "i18n-js";
+import translations from "./translations/pt-BR.json";
+
+const i18n = new I18n(translations);
+i18n.locale = "pt-BR";
+
+export default i18n;
--- src/pages/Home/index.page.tsx
+++ src/pages/Home/index.page.tsx
@@ -1,18 +1,6 @@
 import React from "react";
-// import { Button } from "@mantine/core";
-import { I18n } from "i18n-js";
-import translations from "../../lang/translations/pt-BR.json";
+import i18n from "../../lang";
 
 export default function Home() {
-  const i18n = new I18n(translations);
-  i18n.locale = "pt-BR";
-
-  return <div>teste i18n {i18n.t("subject.name")}</div>;
-
-  // return (
-  //   <div>
-  //     index.page
-  //     <Button>Settings</Button>
-  //   </div>
-  // );
+  return <div>teste i18n {i18n.t("hello")}</div>;
 }
