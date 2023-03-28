ad9b43f97ecd7d8ef9c5609c4361ad9697ef194f
Camila Souza <camila-emanuele@hotmail.com>
2023-03-28 19:52:20 -0300

    install i18n

--- src/pages/Home/index.page.tsx
+++ src/pages/Home/index.page.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { Button } from "@mantine/core";
+// import { Button } from "@mantine/core";
+import { I18n } from "i18n-js";
+import translations from "../../lang/translations/pt-BR.json";
 
 export default function Home() {
-  return (
-    <div>
-      index.page
-      <Button>Settings</Button>
-    </div>
-  );
+  const i18n = new I18n(translations);
+  i18n.locale = "pt-BR";
+
+  return <div>teste i18n {i18n.t("subject.name")}</div>;
+
+  // return (
+  //   <div>
+  //     index.page
+  //     <Button>Settings</Button>
+  //   </div>
+  // );
 }
