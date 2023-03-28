3569b011b638f47143cbdd9c278510b16a4809f9
andresjesse <andres.porfirio.tablet@gmail.com>
2023-03-28 20:17:03 -0300

    configured craco and tsconfig paths (#16)
    Co-authored-by: andres <andres@pop-os.localdomain>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import firebaseConfig from "./config/firebaseConfig";
-import useFirebase from "./hooks/useFirebase";
-import Router from "./router";
+import firebaseConfig from "@/config/firebaseConfig";
+import useFirebase from "@/hooks/useFirebase";
+import Router from "@/router";
 
 function App() {
   const app = useFirebase(firebaseConfig);
--- src/pages/Home/index.page.tsx
+++ src/pages/Home/index.page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import i18n from "../../lang";
+import i18n from "@/lang";
 
 export default function Home() {
   return <div>teste i18n {i18n.t("hello")}</div>;
