c091777cafebf74f305dc731dd17e00ef88ed255
Camila Souza <44167270+camilaesouza@users.noreply.github.com>
2023-04-06 20:16:04 -0300

    @camilaesouza/21/create-app-layout (#27)
    * create app layout and admin index
    
    * renamed i18n messages for layout
    
    * migrated fixed user to useAuth user. added loading check.
    
    * removed unused dark theme check
    
    ---------
    
    Co-authored-by: Andres <andres@pop-os.localdomain>

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "../pages/Home/index.page";
-import Login from "../pages/Login/index.page";
 import ProtectedOutlet from "./_ProtectedOutlet";
+
+import Home from "@/pages/Home/index.page";
+import Login from "@/pages/Login/index.page";
 import Admin from "@/pages/Admin/index.page";
 
 const router = createBrowserRouter([
