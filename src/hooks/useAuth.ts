fb6acaa6e9c81dae264f57673360c106dab4e022
Camila Souza <camila-emanuele@hotmail.com>
2023-05-02 19:04:58 -0300

    upgrade hooks

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -8,15 +8,27 @@ import {
   User,
 } from "firebase/auth";
 
+/**
+ * Firebase authentication hook.
+ * @returns Access to main auth service using email and password strategy, plus user object and loading state flag.
+ */
 export default function useAuth() {
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState<User | null>(null);
 
+  /**
+   * Wrapper for login users with loading state flag for conditional renders.
+   * @param email An active user registered in your firebase project.
+   * @param password User's password.
+   */
   const login = async (email: string, password: string) => {
     setLoading(true);
     await signInWithEmailAndPassword(getAuth(), email, password);
   };
 
+  /**
+   * Wrapper for logout users.
+   */
   const logout = async () => {
     await signOut(getAuth());
   };
--- src/hooks/useFirebase.ts
+++ src/hooks/useFirebase.ts
@@ -1,14 +1,29 @@
 import { useEffect, useState } from "react";
 
 import { FirebaseApp, FirebaseOptions, initializeApp } from "firebase/app";
+import { connectAuthEmulator, getAuth } from "@firebase/auth";
+import { connectFirestoreEmulator, getFirestore } from "@firebase/firestore";
 
+/**
+ * Firebase initialization. This hook should be called in App's entry point.
+ * @param firebaseConfig Your firebase project config for web SDK.
+ * @returns Firebase app when firebase is completely loaded, null otherwise (make sure to conditionally check this in App entrypoint!).
+ */
 export default function useFirebase(firebaseConfig: FirebaseOptions) {
   const [app, setApp] = useState<FirebaseApp | null>(null);
 
   useEffect(() => {
     const app = initializeApp(firebaseConfig);
+
+    if (process.env.NODE_ENV === "test") {
+      console.log("Connecting to emulators...");
+
+      connectAuthEmulator(getAuth(), "http://localhost:9099");
+      connectFirestoreEmulator(getFirestore(), "localhost", 8080);
+    }
+
     setApp(app);
-  }, []);
+  }, [firebaseConfig]);
 
   return app;
 }
