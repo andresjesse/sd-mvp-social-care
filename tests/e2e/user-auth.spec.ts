191ab3a32ab47b1f5feeab9a458aaae33117b5c0
Camila Souza <camila-emanuele@hotmail.com>
2023-06-15 19:47:14 -0300

    add logout and register subject test

--- tests/e2e/user-auth.spec.ts
+++ tests/e2e/user-auth.spec.ts
@@ -16,4 +16,24 @@ test("a registered user can enter the system", async ({ page }) => {
   await expect(page).toHaveURL(/.*admin/);
 });
 
-//TODO: test logout
+test("a registered user can logout the system", async ({ page }) => {
+  await page.goto("http://localhost:3000/login");
+
+  // Text input
+  await page.getByLabel("Email").fill("admin@example.com");
+
+  // Text input
+  await page.getByLabel("Password").fill("123456");
+
+  // Click the enter system button.
+  await page.getByRole("button", { name: "Login" }).click();
+
+  // Expects the URL redirects to admin dashboard.
+  await expect(page).toHaveURL(/.*admin/);
+
+  // Click the logout system button.
+  await page.getByRole("button", { name: "Sair" }).click();
+
+  // Expects the URL redirects to login page.
+  await expect(page).toHaveURL(/.*login/);
+});
